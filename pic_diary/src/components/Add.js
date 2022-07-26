051731f4e6a13f7640c17e5ea2c8fdfe3055f833
TaeHyung KIM <th1122kim@gmail.com>
2022-07-26 16:51:04 +0900

    Add컴포넌트 상단 상태바 기초 구현

--- pic_diary/src/components/Add.js
+++ pic_diary/src/components/Add.js
@@ -1,16 +1,67 @@
-import React,{useState} from 'react';
-import PicAdd from '../UI_components/PicAdd';
-import { dummyData } from '../data/dummyData';
+import React, { useState } from "react";
+import styled from "styled-components";
+import PicAdd from "../UI_components/PicAdd";
+import { dummyData } from "../data/dummyData";
 
 const Add = () => {
   const [picData, setPicData] = useState(dummyData);
+  const Add_container = styled.div`
+    width: 390px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  `;
+  const Info_container = styled.section`
+    height: 76px;
+    background-color: #f4eee2;
+    margin-bottom: 20px;
+    display: flex;
+
+    > .info_wrapper {
+      flex-grow: 5;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: stretch;
+    }
+    > .icon_wrapper {
+      flex-grow: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  `;
+  const Picadd_wrapper = styled.section``;
 
   return (
-    <section className="add">
-        {picData.map((el)=>{
-          return (<PicAdd key={picData.id} picture={el.picture} content={el.picture}></PicAdd>)
-        })}
-    </section>
+    <>
+      <Add_container>
+        <Info_container>
+          <div className="info_wrapper">
+            <div className="date">
+              날짜 :<input></input>
+            </div>
+            <div className="title">
+              제목 :<input></input>
+            </div>
+          </div>
+          <div className="icon_wrapper">
+            <button>사진추가</button>
+          </div>
+        </Info_container>
+        <Picadd_wrapper>
+          {picData.map((el) => {
+            return (
+              <PicAdd
+                key={picData.id}
+                picture={el.picture}
+                content={el.picture}
+              ></PicAdd>
+            );
+          })}
+        </Picadd_wrapper>
+      </Add_container>
+    </>
   );
 };
 
