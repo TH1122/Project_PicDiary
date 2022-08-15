3026ffd5a4b6371f82600df2d2d9ad2677347dda
TaeHyung KIM <th1122kim@gmail.com>
2022-08-15 23:13:33 +0900

    json-server 라이브러리 이용해서 서버통신을 할 수 있게, dummydata가 아닌 json타입의 data를 새로 만들어 주었고, 해당 데이터를 통해 App.js 에서 fetch로 picData를 get 요청으로 받아올 수 있도록 구현

--- pic_diary/src/App.js
+++ pic_diary/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -6,11 +6,26 @@ import MenuBar from "./components/MenuBar";
 import Home from "./components/Home";
 import Add from "./components/Add";
 import List from "./components/List";
-import { dummyData } from "./data/dummyData";
 import AddByDate from "./components/AddByDate";
 
 function App() {
-  const [picData, setPicData] = useState(dummyData);
+  const [picData, setPicData] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/diary/")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setPicData(data);
+      });
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -20,7 +35,10 @@ function App() {
             <MenuBar />
             <section className="main">
               <Routes>
-                <Route path="/" element={<Home picData={picData} />} />
+                <Route
+                  path="/"
+                  element={!isPending && <Home picData={picData} />}
+                />
                 <Route path="/list" element={<List picData={picData} />} />
                 <Route
                   path="/add"
